f847ecc6a9953ad3405f055dd0205e0cc6742931
boslagate-rgb <boslagate@gmail.com>
2026-04-01 18:45:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,25 @@
+const CACHE_NAME = 'bosla-v4';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
 self.addEventListener('install', event => {
-  self.skipWaiting(); // تثبيت فوري إجباري
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting(); 
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim()); // تفعيل فوري
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-  // تلبية شروط جوجل كروم بدون تعقيد
-  event.respondWith(fetch(event.request).catch(() => new Response('Offline')));
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
