6c4471c16ad60af9be7509880d6b546e3e269423
boslagate-rgb <boslagate@gmail.com>
2026-04-01 16:26:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'bosla-offline-v1';
+const CACHE_NAME = 'bosla-offline-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
