95ce740a7b8192eaa99a4eb81e3ec57d08b27b25
boslagate-rgb <boslagate@gmail.com>
2026-04-02 18:34:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,11 @@
-const CACHE_NAME = 'bosla-v9-final-online-preferred';
-
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/offline.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-// Network First + Offline Fallback
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
-      .catch(() => caches.match('/offline.html'))
+      .then(response => response)
+      .catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('/offline.html');
+        }
+      })
   );
 });
